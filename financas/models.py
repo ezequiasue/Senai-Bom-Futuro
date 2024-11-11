3b0fa2f052c79051e53cd390c64eab64e3672ebc
Ezequias S. Santos <phd.esantos@yahoo.com>
2024-11-11 13:31:15 -0400

    Update models.py
    Ajustando Entidades models

--- financas/models.py
+++ financas/models.py
@@ -1,42 +1,43 @@
 # financas/models.py
-
 from django.db import models
 from django.utils import timezone
+from django.db.models import Sum
 
-class Categoria(models.Model):
-    """
-    Modelo para armazenar categorias de receitas e despesas, como "Alimentação", "Salário", "Transporte", etc.
-    """
-    nome = models.CharField(max_length=100, unique=True)  # Nome da categoria
-    descricao = models.TextField(blank=True, null=True)   # Descrição opcional
-
-    def __str__(self):
+class Categorias(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    descricao = models.TextField(blank=True, null=True)
+    
+    def __str__(self) -> str:
         return self.nome
 
     class Meta:
         verbose_name = "Categoria"
-        verbose_name_plural = "Categorias"  # Nome plural para o painel admin
-
+        verbose_name_plural = "Categorias"
 
 class Transacao(models.Model):
-    """
-    Modelo para armazenar as transações financeiras, tanto receitas quanto despesas.
-    """
     TIPOS_TRANSACAO = [
-        ('entrada', 'Entrada'),    # Para receitas
-        ('saida', 'Saída')         # Para despesas
+        ('entrada', 'Entrada'),
+        ('saida', 'Saida')
     ]
-
-    tipo = models.CharField(max_length=7, choices=TIPOS_TRANSACAO)  # Se é uma receita ou despesa
-    valor = models.DecimalField(max_digits=10, decimal_places=2)    # Valor da transação
-    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)  # Categoria da transação
-    descricao = models.TextField(blank=True, null=True)  # Descrição opcional da transação
-    data = models.DateTimeField(default=timezone.now)    # Data e hora da transação
-    saldo_total = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # Saldo após a transação
+    tipo = models.CharField(max_length=7, choices=TIPOS_TRANSACAO)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey('Categorias', on_delete=models.SET_NULL, null=True)
+    descricao = models.TextField(blank=True, null=True)
+    data = models.DateTimeField(default=timezone.now)
+    saldo_total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    pago = models.BooleanField(default=False)
+    data_vencimento = models.DateField(blank=True, null=True)
 
     def __str__(self):
-        return f"{self.get_tipo_display()} - R$ {self.valor} - {self.categoria.nome if self.categoria else 'Sem Categoria'}"
+        return f"{self.get_tipo_display()} - {self.valor} em {self.data.strftime('%Y-%m-%d %H:%M:%S')}"
+
+    @classmethod
+    def calcular_saldo(cls):
+        entradas = cls.objects.filter(tipo='entrada').aggregate(total=Sum('valor'))['total'] or 0
+        saidas = cls.objects.filter(tipo='saida').aggregate(total=Sum('valor'))['total'] or 0
+        return entradas - saidas
 
     class Meta:
         verbose_name = "Transação"
-        verbose_name_plural = "Transações"  # Nome plural para o painel admin
+        verbose_name_plural = "Transacões"
+
