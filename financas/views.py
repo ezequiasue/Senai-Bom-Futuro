22f7f56e455fae8ca2898276729d8a302fcb32de
Ezequias S. Santos <phd.esantos@yahoo.com>
2024-11-11 13:31:37 -0400

    Update views.py

--- financas/views.py
+++ financas/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
+from .models import Transacao, Categorias
+from django.db.models import Sum  # Certifique-se de importar o Sum
 
-def index(request):
-    return render(request, 'index.html') 
-    
-    
-    
-""" from django.shortcuts import render
-from .models import Transacao, Categoria
+def resumo_por_categoria():
+    # Consulta o total por categoria
+    return Transacao.objects.values('categoria__nome').annotate(
+        total=Sum('valor')
+    ).order_by('categoria__nome')
 
 def lista_transacoes(request):
     transacoes = Transacao.objects.all()  # Busca todas as transações
-    categorias = Categoria.objects.all()   # Busca todas as categorias
-    return render(request, 'home.html', {'transacoes': transacoes, 'categorias': categorias}) """
+    categorias = Categorias.objects.all()   # Busca todas as categorias
+    resumo = resumo_por_categoria()  # Obtém o resumo por categoria
+    return render(request, 'index.html', {'transacoes': transacoes, 'categorias': categorias, 'resumo': resumo})
+
+
+def relatorio_categoria(request):
+    """
+    Exibe um relatório de transações agrupadas por categoria.
+    """
+    resumo = resumo_por_categoria()
+    return render(request, 'relatorio_categoria.html', {'resumo': resumo})
